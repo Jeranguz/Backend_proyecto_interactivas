2fb1fbbc82f1edc1c6522dbfaf534df02b209b46
Luisda1828 <vluisdavid4@gmail.com>
2023-11-15 12:51:13 -0600

    The login and registration forms are now displayed depending on what the user wants to do.

--- add_user.php
+++ add_user.php
@@ -44,7 +44,7 @@
                     "email"=> $_POST["email"]
                 ]);
 
-                header("location: index.php");
+                header("location: add_user.php");
             }
 
             }
@@ -52,7 +52,7 @@
     }
 
     function verifyInfo() {
-        global $message; // Para acceder a la variable $message definida fuera de la función
+        global $message; 
         if ($_POST["fullname"] == "") {
             $message = "fullname incorrect";
             return false;
@@ -91,95 +91,108 @@
 <body>
   
       <main class="logs">
-
-    
-         <section id="register" class="login-container">
-           
-            <div class="title-conatiner">
-            <h1 class="add-user-title">Register</h1>
-            </div>
-         
-           <form class="form-container"  method="post" action="add_user.php">
-                        <div class='form-items'>                  
-                                <input id='fullname' class='form-input' type='text' name='fullname' required="">
-                                <label class='' for='fullname'>Fullname</label>
-                        </div>
-                        <div class='form-items'>
-                         
-                                <input id='email' class='form-input' type='text' name='email' required="">
-                                <label class='' for='email'>Email Address</label>                                 
-                          
-                        </div>
-                        <div class='form-items'>
-                                                                             
-                                <input id='username' class='form-input' type='text' name='username' required="">
-                                <label class='' for='username'>Username</label> 
-                           
-                        </div>
-                        <div class='form-items'>                     
-                                <input id='password' class='form-input' type='password' name='password' required="">
-                                <span onclick="togglePassword()"><img class="togglePassword" id="togglePassword" src="./img/eye-password-see-view-svgrepo-com.svg" alt=""></span>
-                                <label class='' for='password'>Password</label>    
-                        </div>
-                        <div class='form-items'>
-                            
-                                <input class="btn-explore register-btn" type='submit' value="Sign up">
-                            
-                        </div>
-                        <p><?php echo $message; ?></p>
-                        <input type="hidden" name="register" value="1">
-                        <p>Do you have an account? <span class="span">Login</span> </p>
-                    </form>
-        </section> 
-
-        <button class="return-bottom">
+        
+      <button class="return-bottom">
             <a href="index.php" class="left-arrow">←</a>
         </button>
-        <section  class="login-container">
+        <?php
+        if($_GET){
+            if($_GET["id"]="register"){
+                echo  
+               
+                "<section id='register' class='login-container'>"
+            ."<div class='title-conatiner'>"
+            ."<h1 class='add-user-title'>Register</h1>"
+            ."</div>"
+         
+           ."<form class='form-container'  method='post' action='add_user.php'>"
+                        ."<div class='form-items'>"                 
+                                ."<input id='fullname' class='form-input' type='text' name='fullname' required=''>"
+                                ."<label class='' for='fullname'>Fullname</label>"
+                        ."</div>"
+                        ."<div class='form-items'>"
+                         
+                                ."<input id='email' class='form-input' type='text' name='email' required=''>"
+                                ."<label class='' for='email'>Email Address</label>"                                 
+                          
+                        ."</div>"
+                        ."<div class='form-items'>"
+                                                                             
+                                ."<input id='username' class='form-input' type='text' name='username' required=''>"
+                                ."<label class='' for='username'>Username</label>" 
+                           
+                        ."</div>"
+                        ."<div class='form-items'>"                     
+                                ."<input id='password' class='form-input' type='password' name='password' required=''>"
+                                ."<span onclick='togglePassword()'><img class='togglePassword' id='togglePassword' src='./img/eye-password-see-view-svgrepo-com.svg' alt=''></span>"
+                                ."<label class=''for='password'>Password</label>"    
+                        ."</div>"
+                        ."<div class='form-items'>"
+                            
+                                ."<input class='btn-explore register-btn' type='submit' value='Sign up'>"
+                            
+                        ."</div>"
+                        ."<p><?php echo $message; ?></p>"
+                        ."<input type='hidden' name='register' value='1'>"
+                        ."<p>Do you have an account? <a class='span' href='add_user.php'> Login  </a> </p>"
+                    ."</form>"
+        ."</section>";     
+            }
+            }else{
+                echo 
+                "<section class='login-container'>"
+                ."<div class='title-conatiner'>"
+                  ."<h1 class='add-user-title'>Login</h1>"
+                ."</div>"
+             
+               ."<form class='form-container'  method='post' action='add_user.php'>"
+                           ."<div class='form-items'>"
+                                   ." <input id='usernameLog' class='form-input' type='text' name='username' required=''>"
+                                    ."<label class='form-label' for='username'>Username</label>"              
+                            ."</div>"
+                            ."<div class='form-items'>"
+                                    ."<input id='password' class='form-input' type='password' name='password' required=''>"
+                                    ."<span onclick='togglePassword()'><img class='togglePassword' id='togglePassword' src='./img/eye-password-see-view-svgrepo-com.svg' alt=''></span>"
+                                    ."<label class='form-label' for='password'>Password</label>"
+                                    ."<div class='flex-row'>"
+                                        ."<div>"
+                                            ."<input type='checkbox'>"
+                                            ."<label>Remember me </label>"
+                                        ."</div>"
+                                        ."<span class='span'>Forgot password?</span>"
+                                    ."</div>"
+                                  
+                            ."</div>"
+                            ."<div class='form-items'>"
+                                    ."<input class='btn-explore register-btn' type='submit' value='Sign In'>"
+                            ."</div>"
+                           ."<input type='hidden' name='login' value='1'>"
+                        ."</form>"
+    
+                        ."<p>Don't have an account? <a class='span' href='add_user.php?id='register''> Sign Up  </a></p>"
+    
+                        ."<p>Or With</p>"
+    
+                            ."<div class='flex-row'>"
+                                ."<a class= 'btn-login' href=''>"
+                                    ."<img class= 'icon-login' src='./img/google-color.svg' alt=''>"
+                                ."</a>" 
+                                ."<a class= 'btn-login' href=''>"
+                                    ."<img class= 'icon-login' src='./img/facebook-color.svg' alt=''>"
+                                ."</a>" 
+    
+                           ."</div>"
+            ."</section>";
+                
+            } 
+        
            
-            <div class="title-conatiner">
-         
-            <h1 class="add-user-title">Login</h1>
-            </div>
-         
-           <form class="form-container"  method="post" action="add_user.php">
-                        <div class='form-items'>
-                                <input id='usernameLog' class='form-input' type='text' name='username' required="">
-                                <label class='form-label' for='username'>Username</label>              
-                        </div>
-                        <div class='form-items'>
-                                <input id='passwordLog' class='form-input' type='password' name='password' required="">
-                                <span onclick="togglePassword()"><img class="togglePassword" id="togglePassword1" src="./img/eye-password-see-view-svgrepo-com.svg" alt=""></span>
-                                <label class='form-label' for='password'>Password</label>
-                                <div class="flex-row">
-                                    <div>
-                                        <input type="checkbox">
-                                        <label>Remember me </label>
-                                    </div>
-                                    <span class="span">Forgot password?</span>
-                                </div>
-                              
-                        </div>
-                        <div class='form-items'>
-                                <input class="btn-explore register-btn" type='submit' value="Sign In">
-                        </div>
-                        <input type="hidden" name="login" value="1">
-                    </form>
 
-                    <p>Don't have an account? <a class="span" href="/add.user.php?id='login'"> Sign Up  </a></p>
 
-                    <p>Or With</p>
 
-                        <div class="flex-row">
-                            <a class= "btn-login" href="">
-                                <img class= "icon-login" src="./img/google-color.svg" alt="">
-                            </a> 
-                            <a class= "btn-login" href="">
-                                <img class= "icon-login" src="./img/facebook-color.svg" alt="">
-                            </a> 
 
-                        </div>
-        </section>
+
+?>
 
       </main>  
 
@@ -188,19 +201,15 @@
             function togglePassword() {
                 const passwordInput = document.getElementById("password");
                 const toggleIcon = document.getElementById("togglePassword");
-                const passwordInput1 = document.getElementById("passwordLog");
-                const toggleIcon1 = document.getElementById("togglePassword1");
 
                 if (passwordInput.type === "password") {
                 passwordInput.type = "text";
-                passwordInput1.type = "text";
                 toggleIcon.src = "./img/eye-password-show-svgrepo-com.svg"; 
-                toggleIcon1.src = "./img/eye-password-show-svgrepo-com.svg"; 
+                
                 } else {
                 passwordInput.type = "password";
-                passwordInput1.type = "password";
                 toggleIcon.src = "./img/eye-password-see-view-svgrepo-com.svg";
-                toggleIcon1.src = "./img/eye-password-see-view-svgrepo-com.svg";
+               
                 }
             }
                /* eyeSwitch */
